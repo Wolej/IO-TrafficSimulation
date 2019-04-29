d3d7c80ca63b097681d1411999eef2384608e243
Wolej <wojciechol98@gmail.com>
2019-04-29 14:10:01 +0200

    ulice

--- src/com/company/MainPanel.java
+++ src/com/company/MainPanel.java
@@ -8,13 +8,18 @@ import static java.lang.Thread.sleep;
 
 public class MainPanel extends JPanel {
     private ArrayList<Intersection> intersections;
+    private ArrayList<Street> streets;
     public MainPanel() {
 
         intersections = new ArrayList<Intersection>();
+        streets = new ArrayList<Street>();
 
         for (int i = 50; i < 3000; i = i + 100)
-            for (int j = 50; j < 3000; j = j + 100)
+            for (int j = 50; j < 3000; j = j + 100) {
                 intersections.add(new Intersection(i, j));
+                streets.add(new Street(i + 10, j + 10, 0));
+                streets.add(new Street(i + 10, j + 10, 1));
+            }
 
 
     }
@@ -41,6 +46,8 @@ public class MainPanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        for (Street s : streets)
+            s.paintStreet(g);
         for (Intersection i : intersections)
             i.paintIntersection(g);
 
--- src/com/company/Street.java
+++ src/com/company/Street.java
@@ -6,7 +6,7 @@ import static java.awt.Color.BLACK;
 
 public class Street {
     private int x, y;
-    private int orientation;
+    private int orientation;        //0 - horizontal, 1 - vertical
     private Intersection lIntersection;
     private Intersection rIntersection;
     public Street(int x, int y, int orientation) {
@@ -18,5 +18,12 @@ public class Street {
     public void paintStreet(Graphics g) {
         g.setColor(BLACK);
 
+        int x1, x2, y1, y2;
+        x1 = x - 2 * orientation;
+        x2 = x + 2 * orientation;
+        y1 = y + 2 - 2 * orientation;
+        y2 = y - 2 + 2 * orientation;
+        g.drawLine(x1, y1, x1 + 100 - 100 * orientation, y1 + 100 * orientation);
+        g.drawLine(x2, y2, x2 + 100 - 100 * orientation, y2 + 100 * orientation);
     }
 }
