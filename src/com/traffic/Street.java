d2fcce75f68143693dadc41a0d3d4542b29d544b
Wolej <wojciechol98@gmail.com>
2019-06-10 11:36:26 +0200

    Małe zmiany

--- src/com/traffic/Street.java
+++ src/com/traffic/Street.java
@@ -1,10 +1,17 @@
 package com.traffic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Street {
     private List<Intersection> intersections;   //List of intersections connected by the street (usually two)
     private List<Line> lines;   //List of traffic lines
+
+    public Street(Intersection l1, Intersection l2) {
+        intersections = new ArrayList<>();
+        intersections.add(l1);
+        intersections.add(l2);
+    }
 
     public int howManyWaiting(Intersection intersection) {
         int res = 0;
